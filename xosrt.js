46dca85afa80ac1e626cced52581e1f7502c2a31
bobrutskovav <thirdstrike@yandex.ru>
2017-04-04 23:21:37 +0300

    Dosent work right now

--- xosrt.js
+++ xosrt.js
@@ -1,56 +1,79 @@
-console.clear();
-console.log('init');
+window.onload = function () {
+	console.clear();
+	console.log('init');
 
-var allCells = document.getElementsByClassName('cell');
-var matrix = [];
+	var allCells = document.getElementsByClassName('cell');
+	var matrix = [];
 
-function fillMatrix() {
-	for (var i = 0; i < allCells.length; i++) {
-		matrix[i] = 0;
-	}
-}
-
-function getFreeCells() {
-	var tempArr = [];
-	for (var i = 0; i < matrix.length; i++) {
-		if (matrix[i] == 0) {
-			tempArr.push(i);
+	function fillMatrix() {
+		for (var i = 0; i < allCells.length; i++) {
+			matrix[i] = 0;
 		}
 	}
-	return tempArr;
-}
+
+	function getFreeCells() {
+		var tempArr = [];
+		for (var i = 0; i < matrix.length; i++) {
+			if (matrix[i] == 0) {
+				tempArr.push(i);
+			}
+		}
+		return tempArr;
+	}
+
+	// function checkWin(m){
+	// 	if(matrix[0]==m && matrix[1] == m && matrix[2] == m){
+	// 		return m;
+	// 	}else if(matrix[3]==m && matrix[4] == m && matrix[5] == m){
+	// 		return m;
+	// 	}else if(matrix[6]==m && matrix[7] == m && matrix[8] == m){
+	// 		return m;
+	// 	}else if(matrix[0] == m && matrix[3] == m && matrix[6] == m){
+	// 		return m;
+	// 	}else if(matrix[1] == m && matrix[4] == m && matrix[7] == m){
+	// 		return m;
+	// 	}else if(matrix[2] == m && matrix[5] == m && matrix[8] == m){
+	// 		return m;
+	// }
 
 
 
-function pcTurn() {
-	var fCells = getFreeCells();
-	console.log(fCells);
-	var randomIndex = Math.floor(Math.random() * (fCells.length - 1));
-	matrix[fCells[randomIndex]] = 2;
-	console.log("PC turns on " + randomIndex);
+	function pcTurn() {
+		var fCells = getFreeCells();
+		console.log(fCells);
+		var randomIndex = Math.floor(Math.random() * (fCells.length - 1));
+		var indexInMatrix = fCells[randomIndex];
+		matrix[indexInMatrix] = 2;
+		console.log("PC turns on id" + (indexInMatrix+1));
+	 var cell =	document.getElementById('id' + (indexInMatrix+1));
+	 cell.style.backgroundColor='yellow';
+	}
+
+	function onClick(e) {
+		var currentColor = e.target.style.backgroundColor;
+		if(currentColor == 'blue' || currentColor == 'yellow' ){
+			console.log("this cell is already checked!");
+			return;
+		}
+		var id = e.target.id;
+		console.log(e);
+		console.log('yep: ' + id);
+		e.target.style.backgroundColor = 'blue';
+		var index = id.split("id")[1] - 1;
+		console.log(index);
+		matrix[index] = 1;
+
+		pcTurn();
+	}
 
 
-}
-
-function onClick(e) {
-	var id = e.target.id;
-	console.log(e);
-	console.log('yep: ' + id);
-	e.target.style.backgroundColor = 'blue';
-	var index = id.split("id")[1] - 1;
-	console.log(index);
-	matrix[index] = 1;
-
-	pcTurn();
-}
-
-
-console.log(allCells);
-fillMatrix();
-console.log(matrix);
+	console.log(allCells);
+	fillMatrix();
+	console.log(matrix);
 
 
 
-for (var i = 0; i < allCells.length; i++) allCells[i].addEventListener('click', function (e) {
-	onClick(e);
-}, true);
+	for (var i = 0; i < allCells.length; i++) allCells[i].addEventListener('click', function (e) {
+		onClick(e);
+	}, true);
+};
